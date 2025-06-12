63ece4663224273996fc2e7b3758e4bbcb62b18f
bkhebert <bondman2004tippy@gmail.com>
2025-06-11 19:54:50 -0500

    ADD AI to list fallacies

---
+++ BackEnd/src/utils/fallacycounter.ts
@@ -0,0 +1,13 @@
+import Fallacy from "../database/models/Fallacy";
+
+const increaseFallacy = (fallacyName) => {
+  let fallacyNameNoSpace = fallacyName.replaceAll(' ', '')
+  switch(fallacyName){
+    case "Appeal to Authority": 
+      
+    break;
+    case "Appeal to Ignorance":
+
+    break;
+  }
+}
