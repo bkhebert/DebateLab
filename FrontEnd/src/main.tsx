86e285e5c0eddd24089e0c81ace96a0646195027
bkhebert <bondman2004tippy@gmail.com>
2025-06-29 16:50:04 -0500

    ADD browser router to maintsx:

--- FrontEnd/src/main.tsx
+++ FrontEnd/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom' // ADD THIS
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+      <BrowserRouter> {/* WRAP APP WITH THIS */}
     <App />
+    </BrowserRouter>
   </StrictMode>,
 )
