c55f003268122b6b170daa00e093abab2cf5ce41
bkhebert <bondman2004tippy@gmail.com>
2025-06-29 15:43:02 -0500

    ADD vite config base route

--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path'; // Needed for proper aliases
 
 export default defineConfig({
+  base: '/',
   plugins: [react(), ],
   build: {
     outDir: 'dist',
