136a0f0d10420c6814c7a2202ed7f17fbc3d3658
bkhebert <bondman2004tippy@gmail.com>
2025-06-28 23:13:29 -0500

    ADD pseudocode

--- Extension/background.js
+++ Extension/background.js
@@ -1,3 +1,5 @@
+
+// Creates a context menu item when the extension is installed
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
     id: "analyze-argument",
@@ -6,8 +8,11 @@ chrome.runtime.onInstalled.addListener(() => {
   });
 });
 
+// When the context menu item is clicked...
 chrome.contextMenus.onClicked.addListener((info, tab) => {
+  // If the menu ID name is 'analyze-argument', AND there is highlighted text
   if (info.menuItemId === "analyze-argument" && info.selectionText) {
+    // Send a message with the action 'analyze text'
     chrome.tabs.sendMessage(tab.id, {
       action: "analyzeText",
       text: info.selectionText
