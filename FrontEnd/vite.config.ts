4d2a546b8aead067c8fd7e4ca5ee8a2600512faf
bkhebert <bondman2004tippy@gmail.com>
2025-06-29 17:36:15 -0500

    undo changes"

--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 import path from 'path'; // Needed for proper aliases
 
 export default defineConfig({
-  base: '',
   plugins: [react(), ],
   build: {
     
